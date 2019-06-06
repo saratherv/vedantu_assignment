e3685c96fefd960d7b73cbac46bbb63ad9ebbf1d
saratherv <saratherv45@gmail.com>
2019-06-06 12:53:54 +0530

    master_commit

---
+++ routes/product.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  Product.find()
+    .select("name price _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          };
+        })
+      };
+      //   if (docs.length >= 0) {
+      res.status(200).json(response);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    quantity : req.body.quantity
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            quantity : result.quantity,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:3000/products/" + result._id
+            }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+module.exports = router;
